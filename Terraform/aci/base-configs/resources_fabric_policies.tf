11bc5644b53dfd91069177749ebf45dbba72e510
scotttyso <tyscott@cisco.com>
2020-11-11 02:58:11 -0500

    added ACI default best practices = startup wizard

--- Terraform/aci/base-configs/resources_fabric_policies.tf
+++ Terraform/aci/base-configs/resources_fabric_policies.tf
@@ -226,3 +226,19 @@ resource "aci_rest" "infra_best_practice" {
 	EOF
 }
 
+resource "aci_rest" "web_interface_timeout" {
+	path       = "/api/node/mo/uni/userext/pkiext/webtokendata.json"
+	class_name = "pkiWebTokenData"
+	payload    = <<EOF
+{
+	"pkiWebTokenData": {
+		"attributes": {
+			"dn": "uni/userext/pkiext/webtokendata",
+			"uiIdleTimeoutSeconds": "65525"
+		},
+		"children": []
+	}
+}
+	EOF
+}
+
