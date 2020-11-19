3eb0a63e4b51c0d84654c5960a156b681960fda8
carello <cp2carello@gmail.com>
2020-11-19 14:06:32 -0600

    cleaning up cache

--- Terraform/aci/base-configs/main.tf
+++
@@ -1,6 +0,0 @@
-provider "aci" {
-	username    = var.aciUser
-	password    = var.aciPass
-	url         = var.aciUrl
-	insecure    = true
-}
--- Terraform/aci/base-configs/resources_Admin.tf
+++
@@ -1,5 +0,0 @@
-resource "aci_pod_maintenance_group" "default" {
-	for_each    = var.admin_maintgroup
-    name  = each.value.name
-    fwtype  = each.value.fwtype
-}
